aef0e9055ccea14a2d2e3f2b4235c41c97ccfabb
bbrauser <65994299+bbrauser@users.noreply.github.com>
2020-08-04 16:09:38 -0500

    PEP8 stylizing

--- lambdata_bbrauser/dataframe_helper.py
+++ lambdata_bbrauser/dataframe_helper.py
@@ -3,12 +3,15 @@ Some function to help cleaning and handling dataframes.
 """
 
 import pandas as pd
+import numpy as np
+
 
 def report_missing_values(df):
     """Print a pretty report of missing values."""
     df = df.isnull().sum()
-    df = pd.DataFrame(df, columns = ['Number Missing'])
+    df = pd.DataFrame(df, columns=['Number Missing'])
     return df
+
 
 def date_split(df):
     df['Month'] = pd.DatetimeIndex(df['Date']).month
