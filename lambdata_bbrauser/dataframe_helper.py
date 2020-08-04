6b9ef7ecb28a665a323d0016b7ca81c8b97764b1
bbrauser <65994299+bbrauser@users.noreply.github.com>
2020-08-04 00:50:55 -0500

    missing values function

--- lambdata_bbrauser/dataframe_helper.py
+++ lambdata_bbrauser/dataframe_helper.py
@@ -6,5 +6,9 @@ import pandas as pd
 
 def report_missing_values(df):
     """Print a pretty report of missing values."""
-    print('Need to write code here!')
+    df = df.isnull().sum()
+    df = pd.DataFrame(df)
+    return df
 
+def tvt_split(df):
+     X_train, y_train, X_val, y_val = train_test_split(train, train_size=0.8, test_size=0.2, random_state=42)
