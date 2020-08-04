3ae2862ad8a88f96ab315ef86fe03e6b476a0f15
bbrauser <65994299+bbrauser@users.noreply.github.com>
2020-08-03 23:45:02 -0500

    version 0.0.3

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ with open("README.md", "r") as file:
 
 setuptools.setup(
     name = "lambdata-bbrauser",
-    version = "0.0.1",
+    version = "0.0.3",
     author = "bbrauser", 
     description = "A collection of Data Science helper functions", 
     long_description = LONG_DESCRIPTION, 
     long_description_content_type = "text/markdown", 
     url = "https://github.com/bbrauser/lambdata", 
     packages = setuptools.find_packages(), 
-    python_requires = ">=3.8", 
+    python_requires = ">=3.6", 
     install_requires = REQUIRED, 
     classifiers = [
         "Programming Language :: Python :: 3", 
